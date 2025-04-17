d5e666ccc18f7d4e6e7da6c1ccc9123ff6e0cdbd
DonLuisM <lfmb03@outlook.com>
2025-04-17 16:48:39 -0500

    Potentials Changes Frontend

--- static/index.js
+++ static/index.js
@@ -1,4 +1,24 @@
-document.getElementById('form').addEventListener('submit', function() {
-    document.querySelector('.loading').style.display = 'block';
-    document.querySelector('.button').style.display = "none";
-});
+
+const form = document.getElementById('form');
+if (form){
+    form.addEventListener('submit', function() {
+        document.querySelector('.loading').style.display = 'block';
+        document.querySelector('.button').style.display = "none";
+    });
+}
+
+const modals = document.querySelectorAll('.modal');
+const btnAbrirModal = document.querySelectorAll('.boton_modal');
+const btnCerrarModal = document.querySelectorAll('.cerrar_modal');
+
+btnAbrirModal.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        modals[index].showModal();
+    })
+})
+
+btnCerrarModal.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        modals[index].close();
+    })
+})
