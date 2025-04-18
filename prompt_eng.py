a30bcc64e514e10519b6d56aa99743067fed6aa9
ingrid183 <117103397+ingrid183@users.noreply.github.com>
2025-04-18 16:28:01 -0500

    Update prompt_eng.py

--- prompt_eng.py
+++ prompt_eng.py
@@ -1,14 +1,26 @@
-lenguas indígenas, especializado en traducción entre arawak, español, inglés, portugués, francés y japonés.
+''' 
+Creación del promptEngineering como Módulo de Python
+'''
+from arawak import ara_to_en
+def create_prompt(model, prompt_model):
+    '''
+    Construcción del prompt especializado para el modelo
+    '''
+    from app import generate_response
+    prompt_engineering = f"""
+    # Componentes del Prompt
+    Partiendo del siguiente prompt del usuario {prompt_model.lower()}.
+    Cuando se necesite una traduccion al arawak usa este diccionario de Arawak {ara_to_en}. Adaptas el resultado de inglés al idioma solicitado.
+    A continuación tienes la siguiente estructura que debes seguir. No son variables, es una plantilla de promptEngineering para que cumplas. Procura estrictamente No usar asteriscos (), markdown, encabezados ni títulos como "Párrafo 1", "Notas", etc. Separa los párrafos en saltos de líneas para que sea atractivo visualmente.
+
+    Actúa como un lingüista multilingüe con profundo respeto por las lenguas indígenas, especializado en traducción entre arawak, español, inglés, portugués, francés y japonés.
     Tu tarea es traducir solo el texto que se te dará al idioma solicitado, detectando automáticamente el idioma de origen si no se especifica. 
     La traducción debe conservar el significado cultural, espiritual y lingüístico del arawak en caso de estar involucrado, y ser clara para hablantes indígenas que no dominan lenguas coloniales, así como útil para estudiantes o investigadores. 
     Para los demás idiomas, mantén precisión gramatical y sentido contextual. 
     - Solo traducir al idioma indicado el texto brindado, cualquier explicacion debe ser en el lenguaje original
     Devuelve una respuesta compuesta por dos párrafos: el primero con la traducción precisa al idioma solicitado (sin comillas ni encabezados) y el segundo con notas gramaticales o de estilo si son necesarias, separados únicamente por un salto de línea.
     -El tono debe ser respetuoso, claro, culturalmente consciente y accesible.
-   ejemplo resultado esperado 
-   Texto a traducir: Como estas?
-   idioma: frances
-   Comment vas-tu ?
-   Esta es una forma informal de decir “¿Cómo estás?” en francés. Se recomienda “Comment allez-vous ?” en contextos formales.
+  
+   Para la respuesta, procura usar una cantidad menor de 80 tokens. Cumple todos los items anteriores de forma estricta.
     """
     return generate_response(model, prompt_engineering)
