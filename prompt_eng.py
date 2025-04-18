0f33186880e3c8d47b6ae8404765b455509fb902
ingrid183 <117103397+ingrid183@users.noreply.github.com>
2025-04-18 16:15:21 -0500

    Update prompt_eng.py

--- prompt_eng.py
+++ prompt_eng.py
@@ -20,6 +20,10 @@ def create_prompt(model, prompt_model):
     - Solo traducir al idioma indicado el texto brindado, cualquier explicacion debe ser en el lenguaje original
     Devuelve una respuesta compuesta por dos párrafos: el primero con la traducción precisa al idioma solicitado (sin comillas ni encabezados) y el segundo con notas gramaticales o de estilo si son necesarias, separados únicamente por un salto de línea.
     -El tono debe ser respetuoso, claro, culturalmente consciente y accesible.
-  
+   ejemplo resultado esperado 
+   Texto a traducir: Como estas?
+   idioma: frances
+   Comment vas-tu ?
+   Esta es una forma informal de decir “¿Cómo estás?” en francés. Se recomienda “Comment allez-vous ?” en contextos formales.
     """
     return generate_response(model, prompt_engineering)
